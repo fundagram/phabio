89bb8bfc20dbdb536ae1a2e47154cd71d5ee0f9a
fundagram <neil@fundagram.com>
2015-11-06 20:41:29 -0700

    Added Zip step to wercker, for S3 push, and some help form Preetam

--- webapp/orbital.py
+++ webapp/orbital.py
@@ -1,9 +1,33 @@
 from flask import Flask
+import traceback
+import orbitalaws as aws 
+import simplejson as json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Welcome to the autmated code pipeline and cloud automation tool.  Developed by neil@orbitalsoftware.ca'
+
+@app.route('/listS3')
+def listS3():
+    try:
+        objectList = aws.listS3
+        return Response(json.dumps(objectList), 200, mimetype='application/json')
+    except:
+        traceback.print_exc()
+        return Response("{}", 400, mimetype='application/json')
+
+
+@app.route('/instance/launch')
+def launch():    
+    try:
+        message = aws.launchEC2Image()
+        return Response(json.dumps(objectList), 200, mimetype='application/json')
+    except:
+        traceback.print_exc()
+        return Response("{}", 400, mimetype='application/json')
+
 
 if __name__ == '__main__':
     app.debug = True
